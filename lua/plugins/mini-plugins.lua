38fcb43b25954c786f15bbc490aee6b1bd346499
debsishu <pauldebsishu2002@gmail.com>
2025-06-14 01:03:49 +0530

    qol: added mini goodies

--- lua/plugins/mini-plugins.lua
+++ lua/plugins/mini-plugins.lua
@@ -30,4 +30,40 @@ return {
             }
         end,
     },
+    {
+        "echasnovski/mini.surround",
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+            custom_surroundings = nil,
+            highlight_duration = 300,
+            mappings = {
+                add = 'sa',            -- Add surrounding in Normal and Visual modes
+                delete = 'ds',         -- Delete surrounding
+                find = 'sf',           -- Find surrounding (to the right)
+                find_left = 'sF',      -- Find surrounding (to the left)
+                highlight = 'sh',      -- Highlight surrounding
+                replace = 'sr',        -- Replace surrounding
+                update_n_lines = 'sn', -- Update `n_lines`
+
+                suffix_last = 'l',     -- Suffix to search with "prev" method
+                suffix_next = 'n',     -- Suffix to search with "next" method
+            },
+            n_lines = 20,
+            respect_selection_type = false,
+            search_method = 'cover',
+            silent = false,
+        },
+    },
+    {
+        "echasnovski/mini.splitjoin",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            local miniSplitJoin = require("mini.splitjoin")
+            miniSplitJoin.setup({
+                mappings = { toggle = "" }, -- Disable default mapping
+            })
+            vim.keymap.set({ "n", "x" }, "sj", function() miniSplitJoin.join() end, { desc = "Join arguments" })
+            vim.keymap.set({ "n", "x" }, "sk", function() miniSplitJoin.split() end, { desc = "Split arguments" })
+        end,
+    },
 }
