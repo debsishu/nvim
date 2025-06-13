a6908f38bb045e842b53d4cf79bc5fa677d8100e
debsishu <pauldebsishu2002@gmail.com>
2025-06-13 17:26:17 +0530

    feat: dropped fzf-lua infavour of telescope

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -16,8 +16,8 @@ return {
                 ---@type snacks.dashboard.Item[]
                 keys = {
                     { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-                    { icon = " ", key = "f", desc = "Find File", action = ":lua require('fzf-lua').files()" },
-                    { icon = " ", key = "g", desc = "Find Text", action = ":lua require('fzf-lua').live_grep_native()" },
+                    { icon = " ", key = "f", desc = "Find File", action = ":lua require('telescope.builtin').find_files()" },
+                    { icon = " ", key = "g", desc = "Find Text", action = ":lua require('telescope.builtin').live_grep()" },
                     { icon = " ", key = "r", desc = "Recent Files", action = ":lua require('fzf-lua').oldfiles()" },
                     { icon = " ", key = "s", desc = "Restore Session", action = ":lua require('persistence').load({ last = true })" },
                     { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,39 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    -- branch = "master", 
+    tag = '0.1.8',
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+        defaults = {
+            file_ignore_patterns = {
+                ".git/",
+                "tmp/",
+                "dryrun/",
+                "node_modules/",
+                "target/",
+                "__pycache__/",
+                ".pytest_cache/",
+                "build/",
+                ".bemol/",
+                ".brazil/",
+                "release-info/",
+                "env/",
+                ".settings/",
+                "logs/",
+                "versionSets/",
+                "packageInfo",
+                "venv",
+                "packageInfo.bak",
+                ".gradle",
+            },
+        },
+    },
+    keys = {
+        { "<leader><space>", function() require('telescope.builtin').find_files() end, desc = "Find Files" },
+        { "<leader>/", function() require('telescope.builtin').live_grep() end, desc = "Grep Files" },
+    }
+}
